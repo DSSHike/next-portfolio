7aeb2af901e9692d948c15f1e8a96fe409bcc114
DSSHike <101084630+DSSHike@users.noreply.github.com>
2023-12-14 17:08:56 +0530

    Script added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +11,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+        {children}
+      
+      
+   
+  <Script src="assets/vendor/purecounter/purecounter_vanilla.js"></Script>
+  <Script src="assets/vendor/aos/aos.js"></Script>
+  <Script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></Script>
+  <Script src="assets/vendor/glightbox/js/glightbox.min.js"></Script>
+  <Script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></Script>
+  <Script src="assets/vendor/swiper/swiper-bundle.min.js"></Script>
+  <Script src="assets/vendor/typed.js/typed.umd.js"></Script>
+  <Script src="assets/vendor/waypoints/noframework.waypoints.js"></Script>
+  <Script src="assets/vendor/php-email-form/validate.js"></Script>
+
+ 
+  <Script src="assets/js/main.js"></Script>
+     
+      </body>
     </html>
   )
 }
